408a5580ce9a8605c09a134f5978e3f1e12f8d39
HeliaTutueanu <150536387+HeliaTutueanu@users.noreply.github.com>
2023-12-08 14:17:16 +0200

    updated movies list+endpoints, added doc.html response/url/return for movies+users

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const bodyParser = require('body-parser');
 
-const topMovies = [
-  { title: 'Movie 1', year: 2020, genre: ['Action', 'Fantasy'] },
-  { title: 'Movie 2', year: 2019, genre: 'Drama' },
+app.use(morgan('dev'));
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+const movies = [
+  { title: 'Movie1',
+  year: 2020,
+  genre: ['Action', 'Fantasy'],
+  description: 'An action and fantasy movie.',
+  director: 'director1',
+  imageUrl: 'movie1.jpg',
+  featured: true },
+  { title: 'Movie2',
+  year: 2019,
+  genre: 'Drama',
+  description: 'A dramatic movie.', 
+  director: 'director2',
+  imageUrl: 'movie2.jpg', 
+  featured: false },
   { title: 'Movie 3', year: 2007, genre: 'Fantasy' },
   { title: 'Movie 4', year: 2013, genre: 'Romance' },
   { title: 'Movie 5', year: 2023, genre: ['Thriller', 'Drama'] },
@@ -15,23 +32,74 @@ const topMovies = [
   { title: 'Movie 10', year: 2021, genre: ['Mystery', 'Drama'] },
 ];
 
-app.use(morgan('dev'));
-app.use(express.static('public'));
-
-app.get('/movies', (req, res) => {
-  res.json({ movies: topMovies });
-});
+const users = [{
+  userID: 'user1',
+  username: 'user123',
+  favorites: ['Movie1'],
+}];
 
 app.get('/', (req, res) => {
     res.send('Welcome to my movie database!');
-  });
+});
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Oh no, it looks like something went wrong!');
 });
 
-  
-app.listen(8080, () => {
-  console.log('This app is listening on port 8080.');
+app.get('/movies', (req, res) => {
+  res.send('List of all movies');
+  res.json(movies);
+});
+
+app.get('/movies/:title', (req, res) => {
+  const title = req.params.title;
+  const movie = movies.find((m) => m.title === title);
+  if (movie) {
+    res.send(`Movie details for ${title}`);
+    res.json(movie);
+  } else {
+    res.status(404).json({ error: 'Movie not found' });
+  }
+});
+
+app.get('/genres/:name', (req, res) => {
+  const name = req.params.name;
+  res.send(`Genre details for ${name}`);
+});
+
+app.get('/directors/:name', (req, res) => {
+  const name = req.params.name;
+  res.send(`Director details for ${name}`);
+});
+
+app.post('/users/register', (req, res) => {
+  res.send('User registration successful');
+});
+
+app.put('/users/update/:userID', (req, res) => {
+  const userID = req.params.userID;
+  res.send(`User information updated for userID: ${userID}`);
+});
+
+app.post('/users/:userID/favorites/add/:movieID', (req, res) => {
+  const userID = req.params.userID;
+  const movieID = req.params.movieID;
+  res.send(`Movie added to favorites for userID: ${userID}`);
+});
+
+app.post('/users/:userID/favorites/remove/:movieID', (req, res) => {
+  const userID = req.params.userID;
+  const movieID = req.params.movieID;
+  res.send(`Movie removed from favorites for userID: ${userID}`);
+});
+
+app.delete('/users/delete/:userID', (req, res) => {
+  const userID = req.params.userID;
+  res.send(`User deregistration successful for userID: ${userID}`);
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
