4493137be51034422c6913f2b434179c2b9f023d
HeliaTutueanu <150536387+HeliaTutueanu@users.noreply.github.com>
2023-12-18 15:57:59 +0200

    allowed localhost path

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ mongoose.connect(connectionURI, { useNewUrlParser: true, useUnifiedTopology: tru
 const Movie = Models.movies;
 const User = Models.users;
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
 app.use(cors({
   origin: (origin, callback) => {
     if (!origin) return callback(null, true);
