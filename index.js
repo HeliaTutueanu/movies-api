923070fef217cb2c477409e15b97c4ceee1f0c97
HeliaTutueanu <150536387+HeliaTutueanu@users.noreply.github.com>
2024-01-12 14:21:05 +0200

    removed allowed origins

--- index.js
+++ index.js
@@ -25,17 +25,7 @@ mongoose.connect(connectionURI, { useNewUrlParser: true, useUnifiedTopology: tru
 const Movie = Models.movies;
 const User = Models.users;
 
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://movies-api-sqg3.onrender.com'];
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 let auth = require('./auth.js')(app);
 const passport = require('passport');
